c1f53b63180a3f31c84ac83bde46e8baeb2a8f24
Darkskittlz (Linux) <darkskiiittles@gmail.com>
2024-02-19 22:54:51 -0500

    set new local variable called focusConfig and set it to the setup function. It works now.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,6 +7,42 @@ vim.g.lazygit = {
   },
 }
 
+local focusConfig = {}
+
+focusConfig.setup = function()
+    require("focus").setup({
+        enable = true, -- Enable module
+        commands = true, -- Create Focus commands
+        autoresize = {
+            enable = true, -- Enable or disable auto-resizing of splits
+            width = 0, -- Force width for the focused window
+            height = 0, -- Force height for the focused window
+            minwidth = 0, -- Force minimum width for the unfocused window
+            minheight = 0, -- Force minimum height for the unfocused window
+            height_quickfix = 10, -- Set the height of quickfix panel
+        },
+        split = {
+            bufnew = false, -- Create blank buffer for new split windows
+            tmux = false, -- Create tmux splits instead of neovim splits
+        },
+        ui = {
+            number = false, -- Display line numbers in the focused window only
+            relativenumber = false, -- Display relative line numbers in the focused window only
+            hybridnumber = false, -- Display hybrid line numbers in the focused window only
+            absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocused windows
+
+            cursorline = true, -- Display a cursorline in the focused window only
+            cursorcolumn = false, -- Display cursorcolumn in the focused window only
+            colorcolumn = {
+                enable = false, -- Display colorcolumn in the focused window only
+                list = '+1', -- Set the comma-separated list for the colorcolumn
+            },
+            signcolumn = true, -- Display signcolumn in the focused window only
+            winhighlight = false, -- Auto highlighting for focused/unfocused windows
+        }
+    })
+end
+
 
 return {
   { "alvan/vim-closetag" },
@@ -19,38 +55,8 @@ return {
   {'JoosepAlviste/nvim-ts-context-commentstring'},
  
   { 'nvim-focus/focus.nvim',
-    version = false,
-    require("focus").setup({
-      enable = true, -- Enable module
-      commands = true, -- Create Focus commands
-      autoresize = {
-        enable = true, -- Enable or disable auto-resizing of splits
-        width = 0, -- Force width for the focused window
-        height = 0, -- Force height for the focused window
-        minwidth = 0, -- Force minimum width for the unfocused window
-        minheight = 0, -- Force minimum height for the unfocused window
-        height_quickfix = 10, -- Set the height of quickfix panel
-      },
-      split = {
-        bufnew = false, -- Create blank buffer for new split windows
-        tmux = false, -- Create tmux splits instead of neovim splits
-      },
-      ui = {
-        number = false, -- Display line numbers in the focussed window only
-        relativenumber = false, -- Display relative line numbers in the focussed window only
-        hybridnumber = false, -- Display hybrid line numbers in the focussed window only
-        absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
-
-        cursorline = true, -- Display a cursorline in the focussed window only
-        cursorcolumn = false, -- Display cursorcolumn in the focussed window only
-        colorcolumn = {
-            enable = false, -- Display colorcolumn in the foccused window only
-            list = '+1', -- Set the comma-saperated list for the colorcolumn
-        },
-        signcolumn = true, -- Display signcolumn in the focussed window only
-        winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
-      }
-    })
+    version = '*',
+    config = focusConfig
   },
 
   {
