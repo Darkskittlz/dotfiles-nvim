d20f77dd2c31e4cd10ad523c5babffbbc041735f
Tristan <darkskiiittles@gmail.com>
2025-10-22 19:44:43 -0600

    commit

--- lua/utils/git_picker.lua
+++ lua/utils/git_picker.lua
@@ -396,7 +396,7 @@ function M.git_branch_picker_with_mode(selected_branch, mode_index)
           if vim.api.nvim_buf_is_valid(buf) then
             -- Update buffer with spinner and message on a single line
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, {
-              "" .. spinner_frames[frame] .. " Pushing to " .. branch
+              " " .. spinner_frames[frame] .. " Pushing to " .. branch
             })
             -- Move to the next frame
             frame = (frame % #spinner_frames) + 1
